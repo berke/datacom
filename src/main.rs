c12ba9b273448e623eebeba3b7620bb5e5448fab
Berke Durak <bd@exhrd.fr>
2020-01-01 17:08:06 +0100

    Seems to work.  Does it mean anything?

--- src/main.rs
+++ src/main.rs
@@ -91,18 +91,36 @@ fn htri(xy0:(u32,u32),k:[u32;4],tr:&mut [u64;N]) {
     tr[0] = q(xy);
 }
 
+fn hd(k1:&[u32],k2:&[u32])->u32 {
+    let mut d = 0;
+    let m = k1.len();
+    for i in 0..m {
+	d += (k1[i]^k2[i]).count_ones();
+    }
+    d
+}
+
+fn hd64(k1:&[u64],k2:&[u64])->u32 {
+    let mut d = 0;
+    let m = k1.len();
+    for i in 0..m {
+	d += (k1[i]^k2[i]).count_ones();
+    }
+    d
+}
+
 fn main() {
     let mut xw = Xorwow::new();
     let mut k1 = [0_u32;4];
     let mut k2 = [0_u32;4];
 
-    k1 = [0xdeadbe88, 0x0badcafe, 0x12345678, 0x9abcdef0];
+    k1 = [0xdeadbe55, 0x0badcafe, 0x12345678, 0x9abcdef0];
 
     const B : usize = 64;
     const M : usize = N * B;
-    const K : usize = 256;
-    const Q : usize = 10;
-    const RN : usize = 2;
+    const K : usize = 16;
+    const Q : usize = 100000;
+    const RN : usize = 4;
     
     let mut x0 = 0;
     let mut y0 = 0;
@@ -111,33 +129,40 @@ fn main() {
     let mut tr = [0_u64;N];
     let mut tri = [0_u64;N];
     let mut sums = Vec::new();
-    sums.resize(K,[[[[0_i64;2];2];M];M]);
+    //sums.resize(K,[[[[0_i64;2];2];M];M]);
+    sums.resize(K,[0_i64;2*2*RN*B*RN*B]);
 
     let mut v = [0.0_f64;K];
-    for k in 0..K {
-    // let ks = [0x88,0x20,0x77,0xff,0x21,0x30,0xcc,0xa0,0x80];
-    // for k0 in 0..ks.len() {
+    let mut d_hist = [0_usize;N*B];
+    for k0 in 0..K {
+	let k = k0 ^ 0x5;
+	// println!("K {:02X}",k);
+	// let ks = [0x88,0x20,0x77,0xff,0x21,0x30,0xcc,0xa0,0x80];
+	// for k0 in 0..ks.len() {
 	// let k = ks[k0];
 	// let k = k0 ^ (k1[0] & 0xff) as usize;
 	k2[0] =
-	      (xw.next() & 0x00000f00)
-	    | (k1[0]     & 0xfffff000)
+	    (xw.next() & 0xfffffff0)
+	    // | (k1[0]     & 0xfffff000)
 	    | (k as u32); // (xw.next() << 8) | (k as u32);
-	// k2[1] = xw.next();
-	// k2[2] = xw.next();
-	// k2[3] = xw.next();
-	k2[1] = k1[1];
-	k2[2] = k1[2];
-	k2[3] =
-	      (xw.next() & 0xff000000)
-	    | (k1[3]     & 0x00ffffff);
+	k2[1] = xw.next();
+	k2[2] = xw.next();
+	k2[3] = xw.next();
+	// k2[1] = k1[1];
+	// k2[2] = k1[2];
+	// k2[3] =
+	//     (xw.next() & 0xfffff000)
+	//     | (k1[3]     & 0x00000fff);
 
 	x0 = 0;
 	y0 = 0;
+	let d = hd(&k1,&k2);
+	d_hist[d as usize] += 1;
+	let mut std = 0;
 	for _ in 0..Q {
 	    let xy = (x0,y0);
 	    let ab = h(xy,k1);
-	    // let _ = htr(xy,k1,&mut tr1);
+	    let _ = htr(xy,k1,&mut tr1);
 	    // let _ = htri(ab,k1,&mut tri1);
 	    // println!("XY={:08X}{:08X} AB={:08X}{:08X}",xy.0,xy.1,ab.0,ab.1);
 	    // for r in 0..N { print!(" {:016X}",tr1[r]); } println!("");
@@ -145,19 +170,26 @@ fn main() {
 
 	    htr(xy,k2,&mut tr);
 	    htri(ab,k2,&mut tri);
+	    // let td = hd64(&tr1,&tr);
+	    // std += td;
 	    // let mut s = 0;
-		// s += ((tr1[r].0^tr2[r].0).count_ones() +
-		//       (tr1[r].1^tr2[r].1).count_ones()) as u64;
+	    // s += ((tr1[r].0^tr2[r].0).count_ones() +
+	    //       (tr1[r].1^tr2[r].1).count_ones()) as u64;
+	    let mut j = 0;
+	    let s = &mut sums[k];
 	    for r1 in 0..RN {
 		let t1 = tr[r1];
 		for r2 in 0..RN {
 		    let t2 = tri[r2];
+		    let mut c1 = t1;
 		    for i1 in 0..B {
-			let c1 = ((t1 >> i1) & 1) as usize;
+			let mut c2 = t2;
 			for i2 in 0..B {
-			    let c2 = ((t2 >> i2) & 1) as usize;
-			    sums[k][r1 * B + i1][r2 * B + i2][c1][c2] += 1;
+			    s[(j+((c1&1)<<1)+(c2&1)) as usize] += 1;
+			    c2 >>= 1;
+			    j += 4;
 			}
+			c1 >>= 1;
 		    }
 		}
 	    }
@@ -171,24 +203,34 @@ fn main() {
 	// 	     sums[k][i1][i1][1][0],
 	// 	     sums[k][i1][i1][1][1]);
 	// }
+	let mut j = 0;
+	let s = &mut sums[k];
+	let mut se = 0.0;
 	for i1 in 0..M {
 	    for i2 in 0..M {
-		let s = &sums[k][i1][i2];
-		let t = s[0][0] + s[0][1] + s[1][0] + s[1][1];
+		let t = s[j+0] + s[j+1] + s[j+2] + s[j+3];
 		let t = t as f64;
-		let f = |i:usize,j:usize| {
-		    let c = s[i][j];
+		let mut f = |ij:usize| {
+		    let c = s[j+ij];
 		    if c == 0 {
 			0.0
 		    } else {
 			let p = c as f64 / t;
-			-p*p.log2()
+			let e = -p*p.log2()-0.25;
+			se += e;
+			e
 		    }
 		};
-		v[k] += f(0,0)+f(0,1)+f(1,0)+f(1,1);
+		v[k] += f(0)+f(1)+f(2)+f(3);
+		j += 4;
 	    }
 	}
-	// println!("{:02X} {} {}",k,v[k],if k as u32 == k1[0]&255 {"*"} else {""});
+	println!("{:02X} {:12.3} {} {:7.5}",k,v[k],if k as u32 == k1[0]&255 {"*"} else {" "},se/(4*M*M) as f64);
+    }
+    for i in 0..N*B {
+	if d_hist[i] > 0 {
+	    println!("{:3} {}",i,d_hist[i]);
+	}
     }
     let mut idx = (0..K).collect::<Vec<usize>>();
     idx.sort_by(|&i, &j|
