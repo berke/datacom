44543ffc708bafa78f544f6373114d29a0da58a0
Berke Durak <bd@exhrd.fr>
2020-01-12 22:06:30 +0100

    New attempt

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::cmp::Ordering;
 
-const N : usize = 2;
+const N : usize = 4;
 
 struct Xorwow {
     a:u32,
@@ -117,18 +117,27 @@ fn hd64(k1:&[u64],k2:&[u64])->u32 {
     d
 }
 
+fn mix(b:usize,n:usize,x:u32,y:u32)->u32 {
+    let mask = ((1 << n) - 1) << b;
+    (x & mask) | (y & !mask)
+}
+
 fn main() {
     let seed = 99123411;
     let mut xw = Xorwow::new(seed);
+    let mut xw2 = Xorwow::new(seed);
+    let mut xw3 = Xorwow::new(seed);
     let mut k1 = [0_u32;4];
     let mut k2 = [0_u32;4];
 
-    k1 = [0xdeadbe33, 0x0badcafe, 0x12345678, 0x9abcdef0];
+    k1 = [0xde33adb2, 0x0badcafe, 0x12345678, 0x9abcdef0];
 
     const B : usize = 64;
     const M : usize = N * B;
-    const K : usize = 1 << 30;
-    const Q : usize = 10000;
+    const LOG2K : usize = 8;
+    const K : usize = 1 << LOG2K;
+    const Q : usize = 100;
+    const P : usize = 100;
     const RN : usize = N;
     
     let mut x0 = 0;
@@ -137,139 +146,109 @@ fn main() {
     let mut tri1 = [0_u64;N];
     let mut tr = [0_u64;N];
     let mut tri = [0_u64;N];
-    // let mut sums = Vec::new();
-    //sums.resize(K,[[[[0_i64;2];2];M];M]);
-    //sums.resize(K,[0_i64;2*2*RN*B*RN*B]);
 
-    let mut v = Vec::new();//[0.0_f64;K];
+    let mut v = [0_i64;K];
     let mut d_hist = [0_usize;N*B];
+    let mut d_min = 0;
     let mut s = [0_i64;2*2*RN*B*RN*B];
-    for k0 in 0..K {
-	for i in 0..s.len() {
-	    s[i] = 0;
-	}
-	// xw.reset(seed);
-	//let k = (((k0 as u32) ^ k1[0]) & (K as u32 - 1)) as usize;
-	let k = (
-	    (if k0 == 0 { k1[0] } else { xw.next() }) & (K as u32 - 1)) as usize;
-	// let k = k0;
-	// println!("K {:02X}",k);
-	// let ks = [0x88,0x20,0x77,0xff,0x21,0x30,0xcc,0xa0,0x80];
-	// for k0 in 0..ks.len() {
-	// let k = ks[k0];
-	// let k = k0 ^ (k1[0] & 0xff) as usize;
-	k2[0] =
-	    // ((  (  xw.next() & 0xff000000
-	    // 	| k1[1]      & 0x00ffffff) ) &
-	    (xw.next() & !(K as u32 - 1))
-	    // | (k1[0]     & 0xfffff000)
-	    | (k as u32); // (xw.next() << 8) | (k as u32);
-	k2[1] = xw.next();
-	k2[2] = xw.next();
-	k2[3] = xw.next();
-	// k2[1] = k1[1];
-	// k2[2] = k1[2];
-	// k2[3] =
-	//       (xw.next() & 0x00000000)
-	//     | (k1[3]     & 0xffffffff);
+    let mut k_closest = [0_u32;4];
+    let mut first = true;
+    loop {
+        for k0 in 0..K {
+            // if (k0 & 255) == 0 || true { println!(" {}/{}",k0,K); }
+            let k = k0;
+            // x0 = 0;
+            // y0 = 0;
+            //xw.reset(seed);
+            // let mut v_tot = 0.0;
+            let mut v_min = [0_i64,0_i64];
+            for p in 0..P {
+                k2[0] = mix(0,LOG2K,k as u32,xw.next());
+                // k2[0] = mix(0,LOG2K,k as u32,mix(LOG2K,8,xw.next(),k1[0]));
+                k2[1] = k1[1];//xw.next();
+                k2[2] = k1[2];//xw.next();
+                k2[3] = mix(0,8,xw.next(),k1[3]);
+                let d = hd(&k1,&k2);
+                d_hist[d as usize] += 1;
+                if first || d < d_min {
+                    d_min = d;
+                    k_closest = k2;
+                }
 
-	x0 = 0;
-	y0 = 0;
-	let d = hd(&k1,&k2);
-	d_hist[d as usize] += 1;
-	let mut std = 0;
-	for _ in 0..Q {
-	    x0 = xw.next();
-	    y0 = xw.next();
-	    let xy = (x0,y0);
-	    let ab = h(xy,k1);
-	    // let _ = htr(xy,k1,&mut tr1);
-	    // let _ = htri(ab,k1,&mut tri1);
-	    // println!("XY={:08X}{:08X} AB={:08X}{:08X}",xy.0,xy.1,ab.0,ab.1);
-	    // for r in 0..N { print!(" {:016X}",tr1[r]); } println!("");
-	    // for r in 0..N { print!(" {:016X}",tri1[r]); } println!("");
+                xw2.reset(seed);
+                for q in 0..Q {
+                    x0 = xw2.next();
+                    y0 = xw2.next();
+                    let xy = (x0,y0);
+                    for r in 0..2 {
+                        let ab =
+                            if r == 0 {
+                                h(xy,k1)
+                            } else {
+                                (xw3.next(),
+                                 xw3.next())
+                            };
+                        // let _ = htr(xy,k1,&mut tr1);
+                        // let _ = htri(ab,k1,&mut tri1);
+                        // println!("XY={:08X}{:08X} AB={:08X}{:08X}",xy.0,xy.1,ab.0,ab.1);
+                        // for r in 0..N { print!(" {:016X}",tr1[r]); } println!("");
+                        // for r in 0..N { print!(" {:016X}",tri1[r]); } println!("");
 
-	    htr(xy,k2,&mut tr);
-	    htri(ab,k2,&mut tri);
-	    // let td = hd64(&tr1,&tr);
-	    // std += td;
-	    // let mut s = 0;
-	    // s += ((tr1[r].0^tr2[r].0).count_ones() +
-	    //       (tr1[r].1^tr2[r].1).count_ones()) as u64;
-	    let mut j = 0;
-	    // let s = &mut sums[k];
-	    for r1 in 0..RN {
-		let t1 = tr[r1];
-		for r2 in 0..RN {
-		    let t2 = tri[r2];
-		    let mut c1 = t1;
-		    for i1 in 0..B {
-			let mut c2 = t2;
-			for i2 in 0..B {
-			    s[(j+((c1&1)<<1)+(c2&1)) as usize] += 1;
-			    c2 >>= 1;
-			    j += 4;
-			}
-			c1 >>= 1;
-		    }
-		}
+                        htr(xy,k2,&mut tr);
+                        htri(ab,k2,&mut tri);
+                        let v = hd64(&tr,&tri);
+                        //if q == 0 || v < v_min {
+                        v_min[r] += v as i64;
+                        //}
+                        // x0 = x0.wrapping_add(123408211);
+	                // y0 = y0.wrapping_add(585748839);
+	            }
+                }
+            }
+	    v[k] += (v_min[0] - v_min[1]); // .abs();
+            first = false;
+        }
+        let mut col = 0;
+        for i in 0..N*B {
+            if d_hist[i] > 0 {
+                print!("{:3} {:8}",i,d_hist[i]);
+                col += 1;
+                if col == 5 {
+                    println!();
+                    col = 0;
+                }
 	    }
-	    // y0 = y0.wrapping_add(1);
-	}
-	// for i1 in 0..M {
-	//     println!("{} {} {} {} {}",
-	// 	     i1,
-	// 	     sums[k][i1][i1][0][0],
-	// 	     sums[k][i1][i1][0][1],
-	// 	     sums[k][i1][i1][1][0],
-	// 	     sums[k][i1][i1][1][1]);
-	// }
-	let mut j = 0;
-	let mut se = 0.0;
-	let mut v_tot = 0.0;
-	for i1 in 0..M {
-	    for i2 in 0..M {
-		let t = s[j+0] + s[j+1] + s[j+2] + s[j+3];
-		let t = t as f64;
-		let mut f = |ij:usize| {
-		    let c = s[j+ij];
-		    if c == 0 {
-			0.0
+        }
+        if col > 0 {
+            println!();
+        }
+        println!("K_closest: {:08X} {:08X} {:08X} {:08X}",
+                 k_closest[0],
+                 k_closest[1],
+                 k_closest[2],
+                 k_closest[3]);
+        println!("     diff: {:08X} {:08X} {:08X} {:08X}",
+                 k_closest[0]^k1[0],
+                 k_closest[1]^k1[1],
+                 k_closest[2]^k1[2],
+                 k_closest[3]^k1[3]);
+        let mut idx = (0..K).collect::<Vec<usize>>();
+        idx.sort_by(|&i, &i|
+                    if v[i] < v[j] {
+                        Ordering::Less
+                    } else if v[i] > v[j] {
+                        Ordering::Greater
 		    } else {
-			let p = c as f64 / t;
-			let e = -p*p.log2()-0.25;
-			se += e;
-			e
+		        Ordering::Equal
 		    }
-		};
-		let w = 1.0-(f(0)+f(1)+f(2)+f(3));
-		//if w > 0.60 {
-		{
-		    // println!("{} {} {}",i1,i2,w);
-		    v_tot += w;
-		}
-		j += 4;
-	    }
-	}
-	v_tot /= (M*M) as f64;
-	v.push(v_tot);
-	println!("{:02X} {:12.7e} {} {:7.5}",k,v_tot,if k as u32 == k1[0]&255 {"*"} else {" "},se/(4*M*M) as f64);
+        );
+        for i in 0..(K - 1).min(25) {
+            let k = idx[i];
+            println!("EST {:3} {:04X} @{:3} {:12} {:12}", i, k,
+                     ((k as u32 ^ k1[0]) & (K - 1) as u32).count_ones(),
+                     v[k], v[idx[i+1]] - v[k]);
+        }
     }
-    for i in 0..N*B {
-	if d_hist[i] > 0 {
-	    println!("{:3} {}",i,d_hist[i]);
-	}
-    }
-    let mut idx = (0..K).collect::<Vec<usize>>();
-    idx.sort_by(|&i, &j|
-		if v[i] < v[j] {
-		    Ordering::Less
-		} else if v[i] > v[j] {
-		    Ordering::Greater
-		} else {
-		    Ordering::Equal
-		}
-    );
     // let mut v_min = v[0];
     // let mut k_min = 0;
     // for k in 1..K {
@@ -279,8 +258,4 @@ fn main() {
     // 	}
     // }
     // println!("EST: {:02X} {}", k_min, v_min);
-    for i in 0..K.min(25) {
-	let k = idx[i];
-	println!("EST {:3} {:02X} {}", i, k, v[k]);
-    }
 }
