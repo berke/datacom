1c5275cf95e931a0319c584fc2e70aebaf0a0b05
Berke Durak <bd@exhrd.fr>
2020-01-14 00:03:48 +0100

    Table.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io::{Read,Write};
 use std::cmp::Ordering;
 
 struct Xorwow {
@@ -49,67 +50,196 @@ fn jn(x:u32,y:u32)->u64 {
     ((x as u64) << 32) | (y as u64)
 }
 
-fn h(x:u64,k:u64)->u64 {
-    x.wrapping_add(k)
-}
+const M : u64 = 0x7fffffff;
+const E : u64 = 3;
 
-fn hi(x:u64,k:u64)->u64 {
-    x.wrapping_sub(k)
-}
-
-pub fn lit(m:usize,w:usize)->f64 {
-    let m = m as f64;
-    (2.0 * w as f64 - m) / ((2.0*m*m.ln().ln()).sqrt())
-}
-
-fn dens(n:usize,x:f64)->f64 {
-  //sqrt(log(log(n as f64))/pi)*exp(-x.^2*log(log(n)));
-    let l = (n as f64).ln().ln();
-    (-x*x*l).exp() * (l/std::f64::consts::PI).sqrt()
-}
-
-fn sample(gseed:u64,kseed:u64,k0:u64,m:usize,n:usize,his:&mut [usize],h0:usize,hshift:u32,hn:usize) {
-    let mut his = &mut his[0..hn];
-    let mut kxw = Xorwow::new(kseed);
-    for j in 0..hn {
-        his[j] = 0;
+fn tbl()->[u32;31] {
+    let mut eks = [0_u32;31];
+    let mut ek = E;
+    for k in 0..31 {
+        eks[k] = ek as u32;
+        ek = (ek * ek) % M;
     }
+    eks
+}
+
+fn f(eks:[u32;31],x:u32)->u32 {
+    let mut q : u64 = 1;
+    let mut y = x;
+    for k in 0..31 {
+        if y == 0 {
+            break;
+        }
+        if y & 1 != 0 {
+            q = (q * eks[k] as u64) % M;
+        }
+        y >>= 1;
+    }
+    (q & 0xffffffff) as u32
+}
+
+fn f0(x:u32)->u32 {
+    let mut q : u64 = 1;
+    let mut y = x;
+    let mut ek = E;
+    for k in 0..31 {
+        if y == 0 {
+            break;
+        }
+        if y & 1 != 0 {
+            q = (q * ek) % M;
+        }
+        y >>= 1;
+        ek = (ek * ek) % M;
+    }
+    (q & 0xffffffff) as u32
+}
+
+fn check_period() {
+    let eks = tbl();
+    let mut seen = Vec::new();
+    let p = 1 << (31 - 6);
+    seen.resize(p,0_u64);
+    for x in 0..0x7fffff_u32 {
+        if x & 0xffffff == 0 {
+            println!("{:5.1}%",100.0*(x as f64)/(0x7fffffff as f64));
+        }
+        //let y = f(eks,x);
+        let y = f0(x);
+        let i = (y >> 6) as usize;
+        let si = seen[i];
+        let j = y & 63;
+        // println!("{:08X} {:08X}",x,y);
+        if (si >> j) & 1 != 0 {
+            println!("SEEN {:08X} {}",y,x);
+            // break;
+        }
+        seen[i] = si | (1 << j);
+    }
+}
+
+fn make_table(xw:&mut Xorwow,t:usize,m:usize,h:u32)->Vec<(u32,u32)> {
+    let mut v = Vec::new();
     for i in 0..m {
-        let mut gxw = Xorwow::new(gseed);
-        let mut p : u64 = 0;
-        let k = kxw.next64();
-        for j in 0..n {
-            let x = gxw.next64();
-            let y = h(x,k0);
-            let x2 = hi(y,k);
-            p += x2.count_ones() as u64;
+        let mut k0 : u32 = xw.next() & M as u32;
+        if k0 == M as u32 {
+            k0 = 0;
         }
-        if p as usize >= h0 {
-            let h = ((p as usize - h0) >> hshift).min(hn-1);
-            his[h] += 1;
+        let mut k = k0;
+        for j in 0..t {
+            k = f0(k) ^ h;
         }
-        
-        // let x = lit(n*64,p) as f64;
-        // let p = dens(n*64,x);
-        // sc += p*x/m as f64; // (x/m as f64 - d).abs();
+        v.push((k,k0));
+    }
+    v
+}
+
+struct Table {
+    vs:Vec<Vec<(u32,u32)>>,
+    hs:Vec<u32>
+}
+
+impl Table {
+    fn new(m:usize)->Self {
+        let mut xw = Xorwow::new(1234567);
+        //let t = ((M as usize / m) as f64).sqrt().floor() as usize;
+        let t = ((100000 as usize / m) as f64).sqrt().floor() as usize;
+        println!("M={} m={} t={}",M,m,t);
+        let mut vs = Vec::new();
+        let mut hs = Vec::new();
+        for k in 0..t {
+            println!("k={}/{}",k,t);
+            let h = k as u32;
+            let mut v = make_table(&mut xw,t,m,h);
+            v.sort_by(|(xa,ya),(xb,yb)| xa.cmp(xb));
+            vs.push(v);
+            hs.push(h);
+        }
+        Table{
+            vs,
+            hs
+        }
+    }
+
+    fn load(path:&str)->Self {
+        let mut fd = std::fs::File::open(path).unwrap();
+        let mut vs = Vec::new();
+        let vs_len = readu64(&mut fd) as usize;
+        for _ in 0..vs_len {
+            let v_len = readu64(&mut fd) as usize;
+            let mut v = Vec::new();
+            for _ in 0..v_len {
+                let x = readu32(&mut fd);
+                let y = readu32(&mut fd);
+                v.push((x,y));
+            }
+            vs.push(v);
+        }
+        let mut hs = Vec::new();
+        let hs_len = readu64(&mut fd) as usize;
+        for _ in 0..hs_len {
+            let h = readu32(&mut fd);
+            hs.push(h);
+        }
+        Table{
+            vs,
+            hs
+        }
+    }
+
+    fn save(&self,path:&str) {
+        let mut fd = std::fs::File::create(path).unwrap();
+        writeu64(&mut fd,self.vs.len() as u64);
+        for v in self.vs.iter() {
+            writeu64(&mut fd,v.len() as u64);
+            for &(x,y) in v.iter() {
+                writeu32(&mut fd,x);
+                writeu32(&mut fd,y);
+            }
+        }
+        writeu64(&mut fd,self.hs.len() as u64);
+        for &h in self.hs.iter() {
+            writeu32(&mut fd,h);
+        }
     }
 }
+
+fn writeu32<T:Write>(fd:&mut T,x:u32) {
+    let mut a = [0_u8;4];
+    a.copy_from_slice(&x.to_ne_bytes());
+    fd.write_all(&a).unwrap();
+}
+
+fn writeu64<T:Write>(fd:&mut T,x:u64) {
+    let mut a = [0_u8;8];
+    a.copy_from_slice(&x.to_ne_bytes());
+    fd.write_all(&a).unwrap();
+}
+
+fn readu32<T:Read>(fd:&mut T)->u32 {
+    let mut a = [0_u8;4];
+    fd.read_exact(&mut a).unwrap();
+    u32::from_le_bytes(a)
+}
+
+fn readu64<T:Read>(fd:&mut T)->u64 {
+    let mut a = [0_u8;8];
+    fd.read_exact(&mut a).unwrap();
+    u64::from_le_bytes(a)
+}
+
+// fn search() {
+
+//     let c = 123456789_u32;
+
+//     for k in 0..t {
+//     }
+// }
+
+//     let m = 10000 as usize;
 
 fn main() {
-    let m = std::env::args().nth(1).unwrap().parse::<usize>().unwrap();
-    let n = std::env::args().nth(2).unwrap().parse::<usize>().unwrap();
-    let mut xw = Xorwow::new(1235);
-    let k0 = xw.next64();
-    let gseed = 92598;
-    let kseed = 31284;
-    let mut his = Vec::new();
-    let hshift = 8;
-    let hn = 64;
-    let hshift = 8;
-    let h0 = 32*n - (hn<<(hshift-1));
-    his.resize(hn,0);
-    sample(gseed,kseed,k0,m,n,&mut his,h0,hshift,hn);
-    for j in 0..hn {
-        println!("{} {}",h0+(j<<hshift),his[j]);
-    }
+    // let tbl = Table::new(100);
+    // tbl.save("rainbow.dat");
+    let tbl = Table::load("rainbow.dat");
 }
